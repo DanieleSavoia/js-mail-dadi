7285e75569f33c1e763dd78766ebd78912616eed
DanieleSavoia <danielesavoia44@gmail.com>
2023-04-17 17:51:18 +0200

    commit2

---
+++ gioco_dei_dadi/script.js
@@ -0,0 +1,23 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+
+
+
+function casuale() {
+    num = Math.round(Math.random() *5+1);
+    document.getElementById("casuale2").innerHTML = num;
+
+}
+
+function casuale() {
+    num = Math.round(Math.random() *5+1);
+    document.getElementById("casuale1").innerHTML = num;
+
+}
+
+
+
+
